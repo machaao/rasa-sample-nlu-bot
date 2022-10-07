7907eea018f0ef1d66ecc58ca9b1dccc1252f871
Aman Verma <112889066+aman0410verma@users.noreply.github.com>
2022-10-07 22:07:29 +0530

    Update local_start.bat

--- local_start.bat
+++ local_start.bat
@@ -2,4 +2,4 @@
 start cmd /k rasa run --debug actions --actions actions
 
 :: Start rasa server with models dir
-start cmd /k rasa run -m models --debug --endpoints config/endpoints.yml --credentials config/credentials-dev.yml
+start cmd /k rasa run -m models --debug --endpoints config/endpoints.yml --credentials config/credentials.yml
